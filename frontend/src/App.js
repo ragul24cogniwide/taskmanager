c2d54a248fcceacc14c132194a0c2eafdba373f5
ragul66 <ragul1.gokul2.3@gmail.com>
2025-04-22 13:46:10 +0530

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Tasks />} />
-          {/* <Route path="task" element={<Tasks />} /> */}
+          {/* Route need to be added */}
         </Route>
       </Routes>
     </Router>
